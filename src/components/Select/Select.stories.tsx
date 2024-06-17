c2ab535c4da042e9bcf4730c2447453cedaf455e
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-06-17 21:32:15 +0300

    feat: done lesson 15

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,72 +1,110 @@
-import { action } from '@storybook/addon-actions';
+import {action} from '@storybook/addon-actions';
 
-import { Select } from './Select';
+import {Select} from './Select';
 import React, {useState} from "react";
+
 
 export default {
     title: 'Select',
     component: Select,
 };
 
-export const CollapsedSelect = () => {
+export const WithValue = () => {
+    const [value, setValue] = useState('2')
+
+
     return (
-        <div>
-            <div onChange={action('click to open')}>Moscow</div>
-        </div>
+            <>
+                <Select value={value}
+                        onChange={setValue}
+                        items={[
+                            {value: '1', title: 'Moscow'},
+                            {value: '2', title: 'Minsk'},
+                            {value: '3', title: 'Kiev'},
+                        ]}/>
+            </>
     )
 }
 
-export const OpenedSelect = () => {
-    const [items, setItems] = useState([
-        {title: 'Moscow', value: 1},
-        {title: 'Minsk', value: 2},
-        {title: 'Kiev', value: 3},
-    ])
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null)
 
     return (
-        <div>
-            <div onChange={action('click to open')}>Moscow</div>
-            {
-                items.map((item, index) => {
-                    if (index !== 0) {
-                        return (
-                            <div>{item.title}</div>
-                        )
-                    }
-                })
-            }
-        </div>
+            <>
+                <Select onChange={setValue}
+                        value={value}
+                        items={[
+                            {value: '1', title: 'Moscow'},
+                            {value: '2', title: 'Minsk'},
+                            {value: '3', title: 'Kiev'},
+                        ]}/>
+            </>
     )
 }
 
-export const SelectDemo = () => {
-    const items = [
-        {title: 'Moscow', value: 1},
-        {title: 'Minsk', value: 2},
-        {title: 'Kiev', value: 3},
-    ]
 
-    const [collapsed, setCollapsed] = useState<boolean>(true)
 
-    const [value, setValue] = useState(items[0])
 
-    return (
-        <div>
-            <div onClick={() => setCollapsed(!collapsed)}>{value.title}</div>
-            {
-                !collapsed && items.map((item, index) => {
-                    const changeValue = () => {
-                        setValue(items[index])
-                        setCollapsed(true)
-                    }
 
-                    if (item.title !== value.title) {
-                        return (
-                            <div key={index} onClick={changeValue}>{item.title}</div>
-                        )
-                    }
-                })
-            }
-        </div>
-    )
-}
+// export const CollapsedSelect = () => {
+//     return (
+//         <div>
+//             <div onChange={action('click to open')}>Moscow</div>
+//         </div>
+//     )
+// }
+//
+// export const OpenedSelect = () => {
+//     const [items, setItems] = useState([
+//         {title: 'Moscow', value: 1},
+//         {title: 'Minsk', value: 2},
+//         {title: 'Kiev', value: 3},
+//     ])
+//
+//     return (
+//         <div>
+//             <div onChange={action('click to open')}>Moscow</div>
+//             {
+//                 items.map((item, index) => {
+//                     if (index !== 0) {
+//                         return (
+//                             <div>{item.title}</div>
+//                         )
+//                     }
+//                 })
+//             }
+//         </div>
+//     )
+// }
+//
+// export const SelectDemo = () => {
+//     const items = [
+//         {title: 'Moscow', value: 1},
+//         {title: 'Minsk', value: 2},
+//         {title: 'Kiev', value: 3},
+//     ]
+//
+//     const [collapsed, setCollapsed] = useState<boolean>(true)
+//
+//     const [value, setValue] = useState(items[0])
+//
+//     return (
+//         <div>
+//             <div onClick={() => setCollapsed(!collapsed)}>{value.title}</div>
+//             {
+//                 !collapsed && items.map((item, index) => {
+//                     const changeValue = () => {
+//                         setValue(items[index])
+//                         setCollapsed(true)
+//                     }
+//
+//                     if (item.title !== value.title) {
+//                         return (
+//                             <div key={index} onClick={changeValue}>{item.title}</div>
+//                         )
+//                     }
+//                 })
+//             }
+//         </div>
+//     )
+// }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,8 @@
 // @flow
 import * as React from 'react';
-import {useState} from "react";
+import {useState, KeyboardEvent, useEffect} from "react";
+import s from './Select.module.css';
+import {log} from "node:util";
 
 type Props = {
     value: any
@@ -20,16 +22,58 @@ const items = [
 ]
 
 export const Select = ({value, items, onChange}: Props) => {
-    const [collapsed, setCollapsed] = useState<boolean>(true)
+    const [active, setActive] = useState<boolean>(false)
+    const [hoveredElValue, setHoveredElValue] = useState(value)
 
-    const onChangeHandler = () => {
-        setCollapsed(!collapsed)
+    const selectedItem = items.find(i => i.value === value)
+    const hoveredItem = items.find(i => i.value === hoveredElValue)
+
+    useEffect( () => {
+        setHoveredElValue(value);
+    }, [ value ])
+
+    const toggleItems = () => {
+        setActive(!active)
+    }
+    const onItemClick = (value: any) => {
+        onChange(value)
+        toggleItems()
+    }
+
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].value === hoveredElValue) {
+                    const pretendentEl = e.key === "ArrowDown" ? items[i + 1] : items[i - 1]
+                    if (pretendentEl) {
+                        onChange(pretendentEl.value)
+                        return
+                    }
+                }
+            }
+            if (!selectedItem) {
+                onChange(items[0].value)
+            }
+        }
+
+        if (e.key === 'Enter' || e.key === "Escape") {
+            setActive(false)
+        }
     }
 
     return (
-        <div>
-            <div onChange={onChangeHandler}>{value}</div>
-            {}
+        <div className={s.select} onKeyUp={onKeyUp} tabIndex={0}>
+            <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+            {
+                active &&
+                <div className={s.items} >
+                    {items.map(i => <div onMouseEnter={() => setHoveredElValue(i.value)}
+                                         className={s.item + " " + (hoveredItem === i ? s.selected : " " )}
+                                         key={i.value}
+                                         onClick={() => onItemClick(i.value)}
+                    >{i.title}</div>)}
+                </div>
+            }
         </div>
     );
 };
