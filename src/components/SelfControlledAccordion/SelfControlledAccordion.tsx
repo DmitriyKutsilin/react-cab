e311df584c1990905a16f2e4c6cbcfe1e62c0b6d
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-06-18 16:15:56 +0300

    feat: done lesson 16

--- src/components/SelfControlledAccordion/SelfControlledAccordion.tsx
+++ src/components/SelfControlledAccordion/SelfControlledAccordion.tsx
@@ -1,6 +1,8 @@
 // @flow
 import * as React from 'react';
-import {useState} from "react";
+import {useReducer, useState} from "react";
+import {action} from "@storybook/addon-actions";
+import {reducer} from "./reducer";
 
 type AccordionProps = {
     title: string
@@ -12,12 +14,14 @@ type AccordionTitleProps = {
 }
 
 export const SelfControlledAccordion = ({title}: AccordionProps) => {
-    const [collapsed, setCollapsed] = useState(true)
+    // const [collapsed, setCollapsed] = useState(false)
+    const [state, dispatch] = useReducer(reducer, {collapsed: false})
 
     return (
         <div>
-            <AccordionTitle title={title} onClick={() => setCollapsed(!collapsed)}/>
-            { !collapsed && <AccordionBody/> }
+            {/*<AccordionTitle title={title} onClick={() => setCollapsed(!collapsed)}/>*/}
+            <AccordionTitle title={title} onClick={() => dispatch({type: "TOGGLE-COLLAPSED"})}/>
+            { !state.collapsed && <AccordionBody/> }
         </div>
     )
 }
