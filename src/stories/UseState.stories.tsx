9d60c1b59206e50a3638394919bcd4babe20a381
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-09-10 13:41:31 +0300

    work with useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'useState demo'
+}
+
+function generateData() {
+    console.log("generateData")
+    //difficult data
+    return 1
+}
+
+export const  Example1 = () => {
+    console.log("Example1")
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state:number) => {
+        return state + 1
+    }
+
+    return (
+        <>
+            <button onClick={() => setCounter(changer)}>+</button>
+            {counter}
+        </>
+    )
+}
